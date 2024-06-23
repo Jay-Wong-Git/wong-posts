4ffc58e4165d9dc90695c04246f431758fc1c1eb
= <=>
2024-06-23 23:03:14 +0800

    optimize image and add page metadata

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,5 +1,18 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+// export const metadata = {
+//   title: "All Posts",
+//   description: "Browse all our posts!",
+// };
+
+export async function generateMetadata() {
+  const numPosts = (await getPosts()).length;
+  return {
+    title: "All Posts",
+    description: `Browse our all ${numPosts} posts!`,
+  };
+}
 
 export default async function FeedPage() {
   const posts = await getPosts();
--- components/posts.js
+++ components/posts.js
@@ -1,15 +1,30 @@
 "use client";
+import Image from "next/image";
 import { useOptimistic } from "react";
 
 import { togglePostLikeStatus } from "@/actions/posts";
 import { formatDate } from "@/lib/format";
 import LikeButton from "./like-icon";
 
+function imageLoader(config) {
+  const urlStart = config.src.split("upload/")[0];
+  const urlEnd = config.src.split("upload/")[1];
+  const transformations = `w_200,q_${config.quality}`;
+  return `${urlStart}upload/${transformations}/${urlEnd}`;
+}
+
 function Post({ post, action }) {
   return (
     <article className="post">
       <div className="post-image">
-        <img src={post.image} alt={post.title} />
+        <Image
+          loader={imageLoader}
+          src={post.image}
+          width={200}
+          height={120}
+          alt={post.title}
+          quality={60}
+        />
       </div>
       <div className="post-content">
         <header>
